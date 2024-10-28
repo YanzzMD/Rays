73aae2aaf01115a3de7ca70e4c4b8d5c76ff62e7
YanzzMD <saturnuus7@gmail.com>
2024-10-28 21:38:11 +0700

    unzipgit

---
+++ settings.js
@@ -0,0 +1,11 @@
+// OBEY COMMANDMENTS
+
+global.prefa = ['','!','.',',','🐤','🗿'] //NOT CHANGE
+global.owner = ['6287888454500'] //OWNER
+global.botname = 'raysBotz' //BOT NAME
+global.versisc = "4.4.4" //NOT CHANGE
+global.baileys1 = require('@whiskeysockets/baileys') //NOT CHANGE
+global.creator = "Rays_Ultraa" //NOT CHANGE
+global.packname = "Sticker By" //OPSIONAL
+global.author = "Rays_Ultraa" //OPSIONAL
+global.simbol = "✇" // SIMBOL MENU
